c88dd5af9679cc97d82644e7df0974ff4efb2c32
Aman Babbar <amanbabber01@gmail.com>
2021-04-03 19:46:50 +0530

    initial commit
    Signed-off-by: Aman Babbar <amanbabber01@gmail.com>

---
+++ app/src/main/java/com/example/conduit/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,42 @@
+package com.example.conduit.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.conduit.R
+import com.example.conduit.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+    private var _binding:FragmentDashboardBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+                ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        _binding = FragmentDashboardBinding.inflate(inflater,container,false)
+        val root = binding.root
+
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
