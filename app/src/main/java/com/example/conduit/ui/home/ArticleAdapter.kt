832f5a03600800b6921e1683283965f70e3a412c
Aman Babbar <amanbabber01@gmail.com>
2021-07-10 12:42:12 +0530

    implemented random image setting on article list item from a list of some vector drawables
    Signed-off-by: Aman Babbar <amanbabber01@gmail.com>

--- app/src/main/java/com/example/conduit/ui/home/ArticleAdapter.kt
+++ app/src/main/java/com/example/conduit/ui/home/ArticleAdapter.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import com.bumptech.glide.Glide
 import com.example.api.models.entity.Article
 import com.example.conduit.R
 import com.example.conduit.databinding.FragmentGlobalFeedBinding
 import com.example.conduit.databinding.ItemArticlesListBinding
+import kotlin.random.Random
 
 class ArticleAdapter(
     val onClickArticle : (article : Article) -> Unit
@@ -27,14 +30,28 @@ class ArticleAdapter(
     }
 ) {
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val binding = ItemArticlesListBinding.bind(view)
         fun bind(article: Article) {
             binding.apply {
                 titleTextView.text = article.title
                 authorTextView.text = article.author.username
                 dateTextView.text = article.createdAt
-                bodyTextView.text = article.body
+                bodyTextView.text = article.description
+                if(!article.author.image.isNullOrEmpty()){
+                    Glide.with(view).load(article.author.image).into(avatarImageView)
+                }
+
+                val drawableList = listOf(R.drawable.vector_random1,
+                    R.drawable.vector_random2,
+                    R.drawable.vector_random3,
+                    R.drawable.vector_random4,
+                    R.drawable.vector_random5,
+                    R.drawable.vector_random6,
+                    R.drawable.vector_random7,
+                    R.drawable.vector_random8,
+                    R.drawable.vector_random9)
+                binding.profileImageView.setImageResource(drawableList.random())
             }
         }
 
